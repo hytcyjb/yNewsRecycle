4c03e90812d85a7334025775a60e06bf8f9d1a6c
yjbo <1457521527@qq.com>
2017-04-03 15:23:59 +0800

    添加注释

--- app/src/main/java/yjbo/yy/ynewsrecycle/main/BackHandledFragment.java
+++ app/src/main/java/yjbo/yy/ynewsrecycle/main/BackHandledFragment.java
@@ -12,6 +12,7 @@ public abstract class BackHandledFragment extends Fragment {
 	 * 所有继承BackHandledFragment的子类都将在这个方法中实现物理Back键按下后的逻辑
 	 * FragmentActivity捕捉到物理返回键点击事件后会首先询问Fragment是否消费该事件
 	 * 如果没有Fragment消息时FragmentActivity自己才会消费该事件
+	 * 注意：FragmentActivity必须与其他的fragment在一个包下；
 	 */
 	protected abstract boolean onBackPressed();
 
