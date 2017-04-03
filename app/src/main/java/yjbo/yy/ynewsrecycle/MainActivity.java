7aff3ba63103fd87b18bdbaddb06e4b35605b777
yjbo <1457521527@qq.com>
2017-04-03 10:57:07 +0800

    添加了主页面底部的布局

--- app/src/main/java/yjbo/yy/ynewsrecycle/MainActivity.java
+++ app/src/main/java/yjbo/yy/ynewsrecycle/MainActivity.java
@@ -2,7 +2,14 @@ package yjbo.yy.ynewsrecycle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+/**
+ * 项目主要完成内容：
+ * 1.完成数据在csdn上的接口展示；（或者GitHub）
+ * 2.完成recycleview的封装在app中的使用；
+ * 3.完成新闻的滑动，动态添加的功能；
+ * @author yjbo
+ * @time 2017/4/3 10:45
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
